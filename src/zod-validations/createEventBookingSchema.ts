64474276b411ee16a6990d1c6c37d492a776c8f6
ARSHDEEP K <arshmobilem2@gmail.com>
2024-05-08 21:52:42 +0530

    chore: refactored API paths ♻️

--- src/zod-validations/createEventBookingSchema.ts
+++ src/zod-validations/createEventBookingSchema.ts
@@ -6,6 +6,8 @@ const createEventBookingSchema = z.object({
     .array(z.object({ name: z.string(), email: z.string().email() }))
     .min(1, { message: "There should be atleast one attendee" }),
   context: z.string().max(200).optional(),
+  eventTypeSlug: z.string(),
+  userId: z.string(),
 });
 
 export default createEventBookingSchema;
